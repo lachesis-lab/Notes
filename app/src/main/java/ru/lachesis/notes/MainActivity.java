7d8314c3a5a63267e7683f61d586458ebc2ba05f
lachesis-lab <mgmmail@mail.ru>
2021-03-20 18:25:35 +0300

    добавила toolbar + первичная обработка нажатий.

--- app/src/main/java/ru/lachesis/notes/MainActivity.java
+++ app/src/main/java/ru/lachesis/notes/MainActivity.java
@@ -3,6 +3,7 @@ package ru.lachesis.notes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,7 +12,10 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +25,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         if (savedInstanceState !=null)
             mNoteId = savedInstanceState.getInt(ARG_NOTE_ID,-1);
-        setContentView(R.layout.activity_main);
         if (savedInstanceState==null) {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -74,6 +81,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+/*
+        switch (item.getItemId()){
+            case R.id.toolbar_menu_item1: {
+                showToast(item);
+                return true;
+            }
+            case R.id.toolbar_menu_item2: {
+                showToast(item);
+                return true;
+            }
+            case R.id.toolbar_menu_item3: {
+                showToast(item);
+                return true;
+            }
+        }
+*/
+        showToast(item);
+        return true;
+    }
 
+    private void showToast(MenuItem item) {
+        Toast toast = Toast.makeText(this,item.getTitle(),Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
